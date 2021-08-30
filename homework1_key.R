8610740be33fe038a82df7b4dd763426b531da47
Brian Evans <bsevans.unc@gmail.com>
2021-08-30 19:35:35 -0400

    homework 1 initial commit

---
+++ homework1_key.R
@@ -0,0 +1,33 @@
+
+# Select station level data:
+
+stations <- 
+  read_csv('data/raw/messy_weather.csv') %>% 
+  dplyr::select(station:name) %>% 
+  distinct()
+
+
+observations <-
+  read_csv('data/raw/messy_weather.csv') %>% 
+  dplyr::select(station, year:march_31) %>%
+    pivot_longer(
+      march_1:march_31,
+      names_to = 'day',
+      values_to = 'value',
+      names_prefix = 'march_') %>% 
+    unite(
+      'date',
+      c(year, month, day),
+      sep = '-') %>% 
+    pivot_wider(
+      names_from = variable,
+      values_from = value) %>% 
+    separate(
+      temperature_min_max,
+      c('temp_min', 'temp_max'),
+      sep = ':') %>% 
+    mutate_at(
+      vars(precip:temp_max),
+      ~as.numeric(.)) %>% 
+    mutate(date = lubridate::as_date(date))
+  
