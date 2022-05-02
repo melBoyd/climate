e4e74a2a9639f7778dbcf149df97c618da8e8a8e
Brian S Evans <bsevans.unc@gmail.com>
2022-05-02 09:20:23 -0400

    removed libraries for example

--- homework1_key.R
+++ homework1_key.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+
 
 # Select station level data:
 
@@ -29,4 +29,5 @@ observations <-
       sep = ':') %>% 
     mutate_at(
       vars(precip:temp_max),
-      ~as.numeric(.))
+      ~as.numeric(.)) %>%
+    mutate(date = as_date(date))
